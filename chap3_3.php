a7930a8c901b703c315864c3dda68c034f223571
kannoayakoX <ayakatsusyu@icloud.com>
2022-02-22 10:25:53 +0900

    課題3を解いた

--- chap3_3.php
+++ chap3_3.php
@@ -1,12 +1,13 @@
 <?php
 foreach (range(1, 100) as $int) 
-    if ($int % 3 == 0 xor $int % 5 == 0) {
-        echo ' Fizz ' . ' ';
-    } elseif(!$int % 3 == 0 and $int % 5 == 0) {
-        echo ' FizzBuzz ' . ' ';
-    } elseif ($int % 5 == 0 xor $int % 3 == 0) { 
-        echo ' Buzz ' . '';        
-    } else {
-        echo $int . ' ';
-    }
+if ($int % 3 == 0 && $int % 5 == 0) {
+    echo ' FizzBuzz ' . ' ';
+} elseif (!$int % 3 == 0 and $int % 5 == 0) {
+    echo ' Buzz ' . ' ';
+} elseif ($int % 3 == 0 xor $int % 5 == 0) { 
+    echo ' Fizz ' . '';        
+} else { 
+    echo $int . ' ';
+}
+  
 
