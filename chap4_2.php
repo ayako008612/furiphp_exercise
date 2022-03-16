e72047e09af8859300a6477ec4b0057c273ab374
kannoayakoX <ayakatsusyu@icloud.com>
2022-03-16 20:46:58 +0900

    chapter4の課題2について

--- chap4_2.php
+++ chap4_2.php
@@ -1,8 +1,18 @@
-<?php
+<<?php
+var_dump("お名前は？");
+$name = trim (fgets(STDIN));
+var_dump("1つ目の金額を入力");
+$item1 = trim( fgets(STDIN));
+var_dump("2つ目の金額を入力");
+$item2 = trim( fgets(STDIN));
+var_dump("3つ目の金額を入力");
+$item3 = trim( fgets(STDIN));
+
 require_once __DIR__ . "/chap4_price.php";
+
 $sum = add($item1,$item2,$item3);
 display_msg($item1,$item2,$item3,$sum);
 
 $price = total_tax($item1,$item2,$item3);
-display_msg($item1,$item2,$item3,$sum);
+display_msg($item1,$item2,$item3,$price);
 ?>
--- chap4_price.php
+++ chap4_price.php
@@ -1,13 +1,4 @@
 <?php
-var_dump("お名前は？");
-$name = trim (fgets(STDIN));
-var_dump("1つ目の金額を入力");
-$item1 = trim( fgets(STDIN));
-var_dump("2つ目の金額を入力");
-$item2 = trim( fgets(STDIN));
-var_dump("3つ目の金額を入力");
-$item3 = trim( fgets(STDIN));
-
 function display_msg($name,$price,$sum){ 
     $msg = <<<EOM
 $name 様
@@ -17,12 +8,12 @@ $sum 円（税込）になります。\n
 EOM;
     echo $msg;
 }
-function add($item1, $item2,$item3) {;
-    $price = ($item1 + $item2 + $item3);
-    return $price ;
+function add($price,$item1,$item2,$item3){
+    $sum = $price + (($item1 + $item2 + $item3) * 0.1);
+    return $sum ;
 }
-function total_tax($item1,$item2,$item3) {
-    $sum = ($item1 + $item2 + $item3) * 1 / 10;
-    return  floor($sum) ;
+function total_tax($item1,$item2,$item3){
+    $price = $item1 + $item2 + $item3;
+    return $price;
 }
 ?>
