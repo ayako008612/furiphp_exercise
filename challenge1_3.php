804da059d925ecec2d171956fd481d45363b00c3
kannoayakoX <ayakatsusyu@icloud.com>
2022-03-27 18:01:28 +0900

    challeng１の課題３foreachをもう一回使う

--- challenge1_3.php
+++ challenge1_3.php
@@ -3,7 +3,10 @@ $team_a = ["勇者", "戦士", "魔法使い"];
 $team_b = ["盗賊", "忍者", "商人"];
 $team_c = ["スライム", "ドラゴン", "魔王"];
 $all_teams = [$team_a, $team_b, $team_c];
-foreach($all_teams as $all_team) {
-    var_dump($all_team);
+$teams = array($all_teams);
+foreach($teams as $team){
+    foreach($all_teams as $all_team ){
+    echo "$all_team\n";
+    }
 }
 ?>
